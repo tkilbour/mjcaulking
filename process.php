1f03d3c73a10ff5e6819e75622615697443ee862
Web Dev <tkilbour@gmail.com>
2024-07-17 13:55:52 -0700

    added site files

---
+++ process.php
@@ -0,0 +1,12 @@
+<?php
+$mailto = "tkilbour@gmail.com";
+$mailto2 = "mandjcaulking@q.com";
+$subject = "Form Results";
+foreach($_POST as $key => $value) {
+	$message .= $key . ': ' . $value;
+	$message .= "\n";
+}
+if(mail($mailto, $subject, $message) && mail($mailto2, $subject, $message)) {
+	header('Location: http://mjcaulking.com/thanks.html');
+}
+?>
